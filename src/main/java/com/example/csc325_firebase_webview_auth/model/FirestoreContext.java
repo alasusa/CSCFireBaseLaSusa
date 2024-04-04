e5f65bf2810acfdbbfe521b8399042e363270875
Aidan LaSusa <114534091+alasusa@users.noreply.github.com>
2024-04-04 08:28:18 -0400

    Update FirestoreContext.java

--- src/main/java/com/example/csc325_firebase_webview_auth/model/FirestoreContext.java
+++ src/main/java/com/example/csc325_firebase_webview_auth/model/FirestoreContext.java
@@ -1,209 +1,32 @@
-package com.example.csc325_firebase_webview_auth.view;//package modelview;
+package com.example.csc325_firebase_webview_auth.model;
 
-import com.example.csc325_firebase_webview_auth.model.Person;
-import com.example.csc325_firebase_webview_auth.viewmodel.AccessDataViewModel;
-import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.QueryDocumentSnapshot;
-import com.google.cloud.firestore.QuerySnapshot;
-import com.google.cloud.firestore.WriteResult;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.UserRecord;
-
-import java.io.File;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.image.ImageView;
-import javafx.stage.FileChooser;
-import javafx.stage.Stage;
+/**
+ *
+ * @author MoaathAlrajab
+ */
+public class FirestoreContext {
 
-public class AccessFBView {
-
-
-    @FXML
-    private TextField nameField;
-    @FXML
-    private Button switchroot;
-    @FXML
-    private TextField majorField;
-    @FXML
-    private TextField ageField;
-    @FXML
-    private Button writeButton;
-    @FXML
-    private Button readButton;
-    @FXML
-    private TextArea outputField;
-    @FXML
-    private ImageView ImageProfile;
-    @FXML
-    private TableView OutPutView;
-    private boolean key;
-    private ObservableList<Person> listOfUsers = FXCollections.observableArrayList();
-    private Person person;
-
-    public ObservableList<Person> getListOfUsers() {
-        return listOfUsers;
-    }
-
-    void initialize() {
-
-        AccessDataViewModel accessDataViewModel = new AccessDataViewModel();
-        nameField.textProperty().bindBidirectional(accessDataViewModel.userNameProperty());
-        majorField.textProperty().bindBidirectional(accessDataViewModel.userMajorProperty());
-        writeButton.disableProperty().bind(accessDataViewModel.isWritePossibleProperty().not());
-    }
-
-    @FXML
-    private void addRecord(ActionEvent event) {
-        addData();
-    }
-
-    @FXML
-    private void readRecord(ActionEvent event) {
-        readFirebase();
-    }
-
-    @FXML
-    private void regRecord(ActionEvent event) {
-        registerUser();
-    }
-
-    public void addData() {
-
-        DocumentReference docRef = App.fstore.collection("References").document(UUID.randomUUID().toString());
-
-        Map<String, Object> data = new HashMap<>();
-        data.put("Name", nameField.getText());
-        data.put("Major", majorField.getText());
-        data.put("Age", Integer.parseInt(ageField.getText()));
-        //asynchronously write data
-        ApiFuture<WriteResult> result = docRef.set(data);
-    }
-
-    public boolean readFirebase() {
-        key = false;
-
-        //asynchronously retrieve all documents
-        ApiFuture<QuerySnapshot> future = App.fstore.collection("References").get();
-        // future.get() blocks on response
-        List<QueryDocumentSnapshot> documents;
+    public Firestore firebase() {
         try {
-            documents = future.get().getDocuments();
-            if (documents.size() > 0) {
-                System.out.println("Outing....");
-                for (QueryDocumentSnapshot document : documents) {
-                    outputField.setText(outputField.getText() + document.getData().get("Name") + " , Major: " +
-                            document.getData().get("Major") + " , Age: " +
-                            document.getData().get("Age") + " \n ");
-                    System.out.println(document.getId() + " => " + document.getData().get("Name"));
-                    person = new Person(String.valueOf(document.getData().get("Name")),
-                            document.getData().get("Major").toString(),
-                            Integer.parseInt(document.getData().get("Age").toString()));
-                    listOfUsers.add(person);
-                }
-            } else {
-                System.out.println("No data");
-            }
-            key = true;
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(getClass().getResourceAsStream("/files/key.json")))
+                   // .setStorageBucket("csc325lasusaweek6demo1.appspot.com")
 
-        } catch (InterruptedException | ExecutionException ex) {
+                    .build();
+            FirebaseApp.initializeApp(options);
+            System.out.println("Firebase is initialized");
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
-        return key;
+        return FirestoreClient.getFirestore();
     }
 
-    public void sendVerificationEmail() {
-        try {
-            UserRecord user = App.fauth.getUser("name");
-            //String url = user.getPassword();
 
-        } catch (Exception e) {
-        }
-    }
-
-    public boolean registerUser() {
-        UserRecord.CreateRequest request = new UserRecord.CreateRequest()
-                .setEmail("user@example.com")
-                .setEmailVerified(false)
-                .setPassword("secretPassword")
-                .setPhoneNumber("+11234567890")
-                .setDisplayName("John Doe")
-                .setDisabled(false);
-
-        UserRecord userRecord;
-        try {
-            userRecord = App.fauth.createUser(request);
-            System.out.println("Successfully created new user: " + userRecord.getUid());
-            return true;
-
-        } catch (FirebaseAuthException ex) {
-            // Logger.getLogger(FirestoreContext.class.getName()).log(Level.SEVERE, null, ex);
-            return false;
-        }
-
-    }
-
-    @FXML
-    private void swapToHTML(){
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/files/WebContainer.fxml"));
-            Parent root = loader.load();
-            Stage stage = (Stage) switchroot.getScene().getWindow();
-            Scene scene = new Scene(root);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
-
- /*@FXML
-    private void selectPicture(ActionEvent event) {
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.getExtensionFilters().addAll(
-                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg"));
-        File selectedFile = fileChooser.showOpenDialog(null);
-        if (selectedFile != null) {
-            uploadPicture(selectedFile);
-        }
-    }
-    public void uploadPicture(File file) {
-        String path = "images/" + UUID.randomUUID() + "-" + file.getName();
-        StorageReference storageRef = FirebaseStorage.getInstance().getReference(path);
-        try {
-            byte[] bytes = Files.readAllBytes(file.toPath());
-            storageRef.putBytes(bytes).addOnSuccessListener(taskSnapshot -> {
-                // Handle successful upload, e.g., getting the URL
-                storageRef.getDownloadUrl().addOnSuccessListener(downloadUrl -> {
-                    System.out.println("Upload success. File URL: " + downloadUrl.toString());
-                    // Optionally, update the ImageView with the new image
-                    Image image = new Image(downloadUrl.toString());
-                    ImageProfile.setImage(image);
-                });
-            }).addOnFailureListener(e -> {
-                // Handle unsuccessful uploads
-                e.printStackTrace();
-            });
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}*/
